2174e41e1d248a56dc608d20adeac731e913bef5
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-02-05 18:52:51 +0000

    Finished Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -49,17 +49,32 @@ public class PracticeProblem {
 		//Write question 4 code here
 		Scanner in = new Scanner(System.in);
 
-		
+		System.out.print("In (int): ");
+		int four = in.nextInt();
+		in.nextLine();
+		System.out.println(four + " times 5 is: " + (four * 5));
+
 	}
 
 	public static void q5() {
 		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Is pizza a sandwich? In (boolean):");
+		boolean five = in.nextBoolean();
+		in.nextLine();
+		System.out.println(five + " is a boolean, and also your answer");
+
 	}
 
 	public static void q6() {
 		//Write question 6 code here
-		
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("In (double): ");
+		double six = in.nextDouble();
+		in.nextLine();
+		System.out.println(six + " minus 3.2 is: " + (six - 3.2));
 	}
 
 }
