567ae0e9b479e5c36595bfc2ed5a4aa089c1a1a9
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-02-05 18:38:34 +0000

    questions 1 through 3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -11,21 +13,42 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
-		
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("In (word): ");
+		String answer = in.nextLine();
+		System.out.println("You inputted: " + answer);
+
 	}
 
 	public static void q2() {
 		//Write question 2 code here
-		
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("In (int 1): ");
+		int one = in.nextInt();
+		in.nextLine();
+		System.out.print("In (int 2): ");
+		int two = in.nextInt();
+		in.nextLine();
+		System.out.println("The quotient rounded down is: " + one/two);
+		System.out.println("The remainder is: " + one%two);
+
 	}
 
 	public static void q3() {
 		//Write question 3 code here
-		
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("In (string): ");
+		String string = in.nextLine();
+		System.out.println("\"" + string + "\"" + " was the text you wrote");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		Scanner in = new Scanner(System.in);
+
 		
 	}
 
